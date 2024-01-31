3438e873b2c52441d8ee836643a259d9849b7f7d
kogaryotaro <ryotaroran531@gmail.com>
2024-01-31 10:00:33 +0900

    1/31

--- regist_complete.php
+++ regist_complete.php
@@ -23,7 +23,48 @@
 
   mb_internal_encoding("utf8");
 
-  $pdo = new PDO("mysql:dbname=regist;host=localhost;", "root", "");
+ try {
+    // ここで接続エラーが発生する可能性があるため、tryブロック内にPDOの処理を含めます。
+    $pdo = new PDO("mysql:dbname=regist;host=localhost;", "root", "");
+ } catch (PDOException $e) {
+    // 接続エラーが発生した場合の処理
+   echo  
+    "<!doctype HTML>
+        <html lang=\"ja\">
+        <head>
+        <meta charset=\"utf-8\">
+        <title>アカウント登録完了画面</title>
+        <link rel=\"stylesheet\" type=\"text/css\" href=\"style2.css\">
+        </head>
+        <body>
+
+        <header>
+            <img src=\"diblog_logo.jpg\">
+            <ul class=\"menu\">
+                <li>トップ</li>
+                <li>プロフィール</li>
+                <li>D.I.Blogについて</li>
+                <li>登録フォーム</li>
+                <li>問い合わせ</li>
+                <li>その他</li>
+                <li><a href=\"regist.php\">アカウント登録</a></li>
+            </ul>
+        </header>
+
+        <h1>アカウント登録完了画面</h1>
+        
+        
+        <div class='error-message'>エラーが発生したためアカウント登録できません</div>
+ 
+
+        <footer>
+            <p>Copyright D.I.works| D.I.blog is the one which provides A to Z about programming</p>
+        </footer>
+
+    </body>
+    </html>";
+    exit();
+ }
 
   $result=  
       $pdo ->exec("insert into regist(family_name, last_name, family_name_kana, last_name_kana, mail, password, gender,  postal_code, prefecture, address_1, address_2, authority, delete_flag, registered_time, update_time)
@@ -58,7 +99,7 @@
   <h1>アカウント登録完了画面</h1>
   
   <?php
-  if ($result !== false) {
+  if ($result !== false || $pdo !== false ) {
     session_unset();
     session_destroy();
   ?>
